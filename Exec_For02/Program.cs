dc77e9665a520a897bc011dd94c8af02fb3e8e3a
yiru00 <eva890706@gmail.com>
2022-10-16 23:02:54 +0800

    作業

---
+++ Exec_For02/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //等腰星形三角形
+        for (int i = 1; i <= 7; i += 2)
+        {
+            int countOfStar = i;
+            int countOfEmpty = (7 - i) / 2;
+            string row = new string(' ', countOfEmpty) + new string('*', countOfStar) + new string(' ', countOfEmpty);
+            Console.WriteLine(row);
+        }
+    }
+}
